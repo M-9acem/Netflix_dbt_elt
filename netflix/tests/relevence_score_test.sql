b3d8c08e0ce418df1fa634867b89bfc9f334f5ac
kacem soulaymane <kacemsoulaymane@kacems-MacBook-Pro.local>
2025-08-01 21:03:56 +0100

    added macros and changed test method

--- netflix/tests/relevence_score_test.sql
+++ netflix/tests/relevence_score_test.sql
@@ -1,8 +1,9 @@
+-- Test for negative or zero relevance scores
 -- SELECT
 --     movie_id,
 --     tag_id,
 --     relevance_score
 -- FROM {{ ref('fct_genome_scores') }}
 -- WHERE relevance_score <= 0
-
+-- Now that we have macros we'll do this instead
 {{ no_nulls_in_columns(ref('fct_genome_scores'))}}
